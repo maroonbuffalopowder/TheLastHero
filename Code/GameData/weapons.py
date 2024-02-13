86df90d1de97d43ed3629599cdd9f6839a69c77f
Alexander Nicolaou <63973827+maroonbuffalopowder@users.noreply.github.com>
2024-02-13 17:59:28 -0500

    Update weapons.py

--- Code/GameData/weapons.py
+++ Code/GameData/weapons.py
@@ -1,7 +1,8 @@
 class Weapon:
   int dmg_type
   # Gives Damage Type(0 For Slashing, 1 For Bludgeoning, 2 For Projectile)
-  int 
+  int prestigeLevel = 0
+  # Gives Prestige Level(000000 Cannot Be Prestige)
   int id_code
   # Gives ID For Code:
   
@@ -17,12 +18,14 @@ class Weapon:
   int crit_chance
   # Gives Crit Chance(1-100)
 
-  int total_kills
+  int total_kills = 0
   # Gives Total Kills With Weapon
-  int total_dmg
+  int total_dmg = 0
   # Gives Total Damage With Weapon
   
-  def Weapon(max_d=7, min_d=4, max_c=2, min_c=3, crit 90):
+  def Weapon(wpn_type = 1, id=000000 ,max_d=7, min_d=4, max_c=2, min_c=3, crit 90):
+    dmg_type = wpn_type
+    id_code = id
     max_dmg = max_d
     min_dmg = min_d
     max_combo = max_c
